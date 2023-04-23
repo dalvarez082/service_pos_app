8f689ad353502b6a8e63576931002965c66ca62b
dalvarez082 <diego980910@gmail.com>
2023-04-23 18:11:26 -0500

    Edicion de data table

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import Table_info from './Components/Table_info';
+import Search_info from './Components/Search_info';
 
 const Client = () => {
-  return (
-<Table_info></Table_info>
+  return (   
+
+    <div>
+
+      <div>
+        <Search_info></Search_info>
+
+      </div>
+
+
+
+      <div style={{
+        height : '80%'
+      }}>
+      <Table_info></Table_info>
+      </div>
+
+     
+
+    </div>
+
+
+
   );
 };
 
