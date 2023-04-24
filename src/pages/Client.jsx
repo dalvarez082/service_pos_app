67eef8c3263e12c5743424ddf12f7f8b2d9e827f
camilasierraO <ospinasierracamila@gmail.com>
2023-04-23 19:01:25 -0500

    Adiciónn 2 de componente en la pagina client

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Table_info from './Components/Table_info';
 import Search_info from './Components/Search_info';
+import Add_info from './Components/Add_info';
+
 
 const Client = () => {
   return (   
@@ -8,8 +10,9 @@ const Client = () => {
     <div>
 
       <div>
+        
         <Search_info></Search_info>
-
+        <Add_info></Add_info>
       </div>
 
 
